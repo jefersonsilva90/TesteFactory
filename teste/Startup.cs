c7ec9b50d31541bc42b62668733c615650c4d930
jefersonsilva <jefersonfs@hotmail.com>
2022-04-13 13:11:31 -0300

    ajuste

--- teste/Startup.cs
+++ teste/Startup.cs
@@ -1,9 +1,14 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Newtonsoft.Json.Linq;
 using teste.Interface;
 using teste.Servicos;
 
@@ -16,7 +21,8 @@ namespace teste
         {
             Configuration = configuration;
         }
-
+        
+        
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -26,7 +32,17 @@ namespace teste
             services.AddTransient<IService, ServicoA>();
             services.AddTransient<IService, ServicoB>();
             services.AddSingleton<Integrador>();
+            
+            services.AddHttpClient("keycloack", httpClient =>
+            {
+                httpClient.BaseAddress = new Uri("http://localhost:8080/realms/myrealm/");
+            });
 
+            services.AddHttpClient("votacao", httpClient =>
+            {
+                httpClient.BaseAddress = new Uri("https://www.criarenquete.com.br/api/");
+            });
+            
             services.AddSwaggerGen(options =>
             {
                 options.SwaggerDoc("v1", new OpenApiInfo
@@ -35,10 +51,35 @@ namespace teste
                     Title = "Teste"
                 });
             });
+            
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+            }).AddJwtBearer(o =>
+            {
+                o.Authority = Configuration["Keycloack:UrlBase"] + Configuration["Keycloack:Authority"];
+                o.Audience = Configuration["Keycloack:ClientId"];
+                o.SaveToken = false;
+                //o.RequireHttpsMetadata = false;
+                //o.IncludeErrorDetails = true;
+                //o.RequireHttpsMetadata = false;
+                o.Events = new JwtBearerEvents()
+                {
+                    OnAuthenticationFailed = c =>
+                    {
+                        c.NoResult();
+                        string errorTextDEfault = "An error occured processing your authentication.";
+                        c.Response.StatusCode = 401;
+                        c.Response.ContentType = "text/plain";
+                        return c.Response.WriteAsync(errorTextDEfault);
+                    }
+                };
+            });
 
 
         }
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
